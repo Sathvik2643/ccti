5684d88bb7502e542ae0715224f4a870af6b1e79
Sathvik2643 <111695889+Sathvik2643@users.noreply.github.com>
2025-12-18 15:13:42 +0530

    main.js

--- main.js
+++ main.js
@@ -1,45 +1,14 @@
-let certificates = [];
-
-fetch("certificates.json")
-  .then(r => r.json())
-  .then(d => certificates = d);
-
 function showSection(id) {
   document.querySelectorAll(".section").forEach(s => s.style.display = "none");
   document.getElementById(id).style.display = "block";
 }
 
 function toggleMenu() {
-  const menu = document.getElementById("navMenu");
-  menu.classList.toggle("show");
+  document.getElementById("navMenu").classList.toggle("show");
 }
 
 function closeMenu() {
-  const menu = document.getElementById("navMenu");
-  if (menu) menu.classList.remove("show");
-}
-
-
-function verifyCert() {
-  const id = document.getElementById("certInput").value.trim().toUpperCase();
-  const res = document.getElementById("certResult");
-  const down = document.getElementById("certDownload");
-  down.innerHTML = "";
-
-  const found = certificates.find(c => c.id === id);
-  if (found) {
-    res.textContent = "✔ VALID Certificate";
-    res.style.color = "green";
-    const a = document.createElement("a");
-    a.href = found.file;
-    a.target = "_blank";
-    a.className = "btn";
-    a.textContent = "Download Certificate";
-    down.appendChild(a);
-  } else {
-    res.textContent = "✖ INVALID Certificate";
-    res.style.color = "red";
-  }
+  document.getElementById("navMenu").classList.remove("show");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
